3e0e7147e8a58cead64828e24e6d8eb0a13fd3b2
your-user-name <your.email@example.com>
2022-05-25 16:13:19 -0600

    what section init

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Grid from "../components/GridSection";
 import HealthCareThree from "../components/HealthCareThree";
 import Hero from "../components/Hero";
 import Socials from "../components/Socials";
+import What from "../components/What";
 import Why from "../components/Why";
 import Footer from "../global/Footer";
 import Navbar from "../global/Nav/Navbar";
@@ -20,6 +21,7 @@ const Home: NextPage = () => {
       <main>
         <Navbar />
         <Hero />
+        <What />
         <Why />
         {/* /* likely delete <GridSection />  */}
         <HealthCareThree />
