98ecb63d615e63536dbafeca1182a1a4b2dea0dd
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-06-06 00:12:37 +0100

    REFACTOR (WhyMedDAO) : refactors the DesktopTabletView
    Uses the new components Card and HeroCard

--- src/components/WhyMedDAO/DesktopTabletView.tsx
+++ src/components/WhyMedDAO/DesktopTabletView.tsx
@@ -1,31 +1,37 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
 import cardsContent from "./content.json";
+import { HeroCard } from "./HeroCard";
+import { Card } from "./Card";
+import { ulid } from "ulid";
+
+type ContentLinkProps = {
+  href: string;
+};
+
+type ContentProps = {
+  title: string;
+  paras?: Array<string>;
+  link?: ContentLinkProps;
+};
 
 export const DesktopTabletView = () => (
   <Box as="section" w="100%" h="fit-content" bgColor="yellow" my="8rem">
-    <Grid as="ul" gridTemplateColumns={{ lg: "1fr 1fr 1fr" }} w="100%" h="100%">
+    <Grid
+      as="ul"
+      gridTemplateColumns={{ md: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+      w="100%"
+      h="100%"
+    >
       {cardsContent &&
-        cardsContent.map((card, index) => {
+        cardsContent.map((card: ContentProps, index: number) => {
           const isLeadCard = index === 0;
-          const isEvenCard = index % 2;
-          return (
-            <GridItem
-              as="li"
-              w="100%"
-              h="20rem"
-              bg={isLeadCard ? "#FF003F" : isEvenCard ? "white" : "#FFF4F7"}
-              colSpan={isLeadCard ? 2 : 1}
-              boxShadow={
-                isLeadCard
-                  ? "none"
-                  : "inset 0px 2.74699px 5.49398px rgba(0, 0, 0, 0.25)"
-              }
-              listStyleType='none'
-              color='black'
-            >
-              {card.title}
-            </GridItem>
-          );
+          const isEvenCard = index % 2 === 0;
+
+          if (isLeadCard) {
+            return <HeroCard key={ulid()} {...card} />;
+          } else {
+            return <Card key={ulid()} {...card} isEvenCard={isEvenCard} />;
+          }
         })}
     </Grid>
   </Box>
