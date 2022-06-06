ba84126c6cf4f64a6db05a1a5fd1e5e509550379
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-06-06 23:57:21 +0100

    FEAT (TextScroll) : adds basic structure
    Flicks between two views desktop and mobile
    
    Adds the two views
    
    Adds hook that disctates when the views should change

---
+++ src/components/TextScroll/MobileView.tsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Image, Heading } from "@chakra-ui/react";
+import Logo from "../Logo";
+
+export const MobileView = () => (
+  <Box bg="blue" h='80vh'>
+    {/* <Flex w="100%" bg="black" justifyContent="center" alignItems="center">
+      <Image
+        src="./AngleBackground.png"
+        maxH="80vh"
+        minW="100%"
+        h="100%"
+        objectFit="cover"
+        zIndex={2}
+      />
+      <Flex maxW="1500px">
+        <Heading as="h2" color="white">
+          Big Test
+        </Heading>
+        <Logo />
+      </Flex>
+    </Flex> */}
+  </Box>
+);
