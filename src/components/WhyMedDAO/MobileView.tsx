a9181df726c584641db7bf16f95831357eada3c6
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-06-06 23:07:06 +0100

    FEAT (MobileView) : adds scrollable carousel animation

--- src/components/WhyMedDAO/MobileView.tsx
+++ src/components/WhyMedDAO/MobileView.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, VStack } from "@chakra-ui/react";
 import Slider from "react-slick";
 import { ulid } from "ulid";
 import cardsContent from "./content.json";
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { HeroCard } from "./HeroCard";
 import { Card } from "./Card";
+import styled from "@emotion/styled";
 
 type ContentLinkProps = {
   href: string;
@@ -17,13 +18,52 @@ type ContentProps = {
   link?: ContentLinkProps;
 };
 
+const StyledSlider = styled(Slider)`
+  & > *.slick-dots {
+    display: flex !important;
+    width: 100vw;
+    position: absolute;
+    bottom: 20px;
+    left: 0px;
+    padding: 0 2rem 0 2rem;
+    & > li {
+      height: 0.5rem;
+      margin-left: 3px;
+      margin-right: 3px;
+      flex: 0 1 25%;
+      border-bottom: 1px solid #231f1f;
+      box-shadow: inset 0 0 0 0 #ff003f;
+    }
+    & > li:first-child,
+    & > li:last-child {
+      margin-left: 0px;
+      margin-right: 0px;
+      flex: 0 1 25%;
+      border-bottom: 1px solid #231f1f;
+    }
+    & > .slick-active {
+      box-shadow: inset 100px 0 0 0 #ff003f;
+      transition: box-shadow ease 5s;
+    }
+  }
+`;
+
 export const MobileView = () => {
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
-    speed: 250,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: false,
+    useCSS: true,
+    customPaging: () => (
+      <VStack justifyContent="flex-end" h="3px">
+        <Box w="25%" h="0px" />
+      </VStack>
+    ),
   };
   const [heroCard, ...cards] = cardsContent;
   return (
@@ -38,7 +78,7 @@ export const MobileView = () => {
         <HeroCard {...heroCard} />
       </GridItem>
       <GridItem w="100vw">
-        <Slider {...settings}>
+        <StyledSlider {...settings}>
           {cards &&
             cards.map((card: ContentProps, index: number) => {
               const isEvenCard = index % 2 === 0;
@@ -51,7 +91,7 @@ export const MobileView = () => {
                 />
               );
             })}
-        </Slider>
+        </StyledSlider>
       </GridItem>
     </Grid>
   );
