1d2b83a09dfa2963c62b38e857896c99726891e9
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-06-07 23:21:31 +0100

    STYLE (Navbar) : adds navbar styling for responsive behavioru

--- src/global/Nav/Navbar.tsx
+++ src/global/Nav/Navbar.tsx
@@ -1,26 +1,32 @@
-import { Box, Button, Flex, Image, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Link, VStack } from "@chakra-ui/react";
 import React from "react";
 import FullBleed from "../Layout/FullBleed";
 
 const Navbar = () => {
   return (
     <FullBleed>
-      <Flex
+      <VStack
         as="nav"
-        width="100%"
+        width="100vw"
         h="5rem"
-        justifyContent="center"
         position="fixed"
         top={0}
+        alignItems="center"
+        justifyContent="center"
         zIndex={10}
         bg="#FFFFFF"
       >
-        <Flex align="center" w="100%" maxW="1500px" grow={1}>
+        <Flex
+          align="center"
+          justifyContent="space-between"
+          w="100%"
+          px='1rem'
+          maxW={{ base: '100%', lg: "1440px" }}
+        >
           <Box w="10.2rem" h="auto">
             <Image src="./MedDAO_Logo_WName.png" alt="MedDAO logo" />
           </Box>
-          <Flex grow={1} justifyContent="flex-end" align="center">
-            {/* 5d5d5d color for links */}
+          <Flex align="center">
             <Link
               color="#5d5d5d"
               fontFamily="Neue Montreal"
@@ -45,12 +51,12 @@ const Navbar = () => {
             >
               Roadmap
             </Link>
+            <Button bg="red" minH="3em" color="white">
+              Join Us
+            </Button>
           </Flex>
-          <Button bg="red" minH="3em" color="white">
-            Join Us
-          </Button>
         </Flex>
-      </Flex>
+      </VStack>
     </FullBleed>
   );
 };
