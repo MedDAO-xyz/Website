2ce8980d0ff5b5e2d20a877ad650a4ed55572a91
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-06-08 00:17:13 +0100

    FEAT (Hero) : add mobile styles for the graphic

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,12 +3,15 @@ import { Fade, Slide } from "react-awesome-reveal";
 import { Grid, GridItem, Box, Heading, VStack } from "@chakra-ui/react";
 import FullBleed from "../../global/Layout/FullBleed";
 import { BgImage } from "./BgImage";
+import { MobileBgImage } from "./MobileBgImage";
+import { useIsMobile } from "../_hooks";
 
 const Hero = () => {
+  const isMobile = useIsMobile();
   return (
     <FullBleed>
       <Grid
-        h="70vh"
+        h={{ sm: "20vh", md: "70vh" }}
         w="100vw"
         alignItems="center"
         justifyContent="center"
@@ -19,24 +22,44 @@ const Hero = () => {
         templateAreas={`"main"`}
         overflow="hidden"
       >
-        <GridItem area={"main"} w="100vw">
-          <BgImage />
+        <GridItem area={"main"} w="100vw" overflow="hidden">
+          {isMobile && <MobileBgImage />}
+          {!isMobile && <BgImage />}
         </GridItem>
         <GridItem area={"main"} w="100vw">
           <VStack justifyContent="center" alignItems="center" w="100vw">
-            <Slide direction="down" triggerOnce>
-              <Heading as="h1" fontSize="3em" mt="1em" color="black">
-                A New
-              </Heading>
-            </Slide>
-            <Fade delay={200} triggerOnce>
-              <Heading as="h1" fontSize="3em" mt=".75em" color="black">
-                Value Layer
-              </Heading>
-              <Heading as="h1" fontSize="3em" mt=".75em" color="black">
-                In Healthcare
-              </Heading>
-            </Fade>
+            {!isMobile && (
+              <>
+                <Slide direction="down" triggerOnce>
+                  <Heading as="h1" fontSize="3em" mt="1em" color="black">
+                    A New
+                  </Heading>
+                </Slide>
+                <Fade delay={200} triggerOnce>
+                  <Heading as="h1" fontSize="3em" mt=".75em" color="black">
+                    Value Layer
+                  </Heading>
+                  <Heading as="h1" fontSize="3em" mt=".75em" color="black">
+                    In Healthcare
+                  </Heading>
+                </Fade>
+              </>
+            )}
+            {isMobile && (
+              <>
+                <Slide direction="down" triggerOnce>
+                  <Heading as="h1" fontSize="3em" color="black">
+                    A New
+                  </Heading>
+                  <Heading as="h1" fontSize="3em" color="black">
+                    Value Layer
+                  </Heading>
+                  <Heading as="h1" fontSize="3em" color="black">
+                    In Healthcare
+                  </Heading>
+                </Slide>
+              </>
+            )}
           </VStack>
           {/* </Flex> */}
         </GridItem>
