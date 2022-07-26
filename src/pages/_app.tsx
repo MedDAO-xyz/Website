1a2aabc1b4426d91d481323f565bb8341f47d36b
compositefellow <compositefellow@protonmail.com>
2022-07-26 11:04:23 -0600

    added id to scripts.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
+        id="googleAnalytics_one"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="googleAnalytics_two" strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
