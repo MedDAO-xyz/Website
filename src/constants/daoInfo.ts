69e68c7312cafa3e807f10cb99c7cc12cc7ad8bb
ryan-meddao <105010559+ryan-meddao@users.noreply.github.com>
2022-07-25 22:56:29 -0400

    Update daoInfo.ts

--- src/constants/daoInfo.ts
+++ src/constants/daoInfo.ts
@@ -60,7 +60,7 @@ We do it because we believe that access to medical knowledge and expertise shoul
     q: "How do I get involved?",
     a: `So we've piqued your interest and now you want to get involved. Maybe you're a healthcare provider and want to truly be part of a vision to utilize blockchain technology and shared ownership to better share and deliver healthcare knowledge. Maybe you are a patient or advocate who wants to take on a more of an active role in how medical expertise is shared. Or perhaps, you are just interested in learning more about or contributing to the conversation on Web3, blockchain and DAOs as they pertain to healthcare.
     
-If you're interested in learning more you can find the core MedDAO documents on our Gitbook linked here. We've also linked to several of our favorite articles about DAOs, Web3 and healthcare.
+If you're interested in learning more you can find the core MedDAO documents on our GitBook linked here. We've also linked to several of our favorite articles about DAOs, Web3 and healthcare.
 
 We encourage you to sign up for our email list and follow our socials to receive updates regarding progress of MedDAO and, community events.
 
