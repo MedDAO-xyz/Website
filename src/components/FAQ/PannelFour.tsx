83d312efada676d1d9ad59805eeff05f9879eb29
ryan-meddao <105010559+ryan-meddao@users.noreply.github.com>
2022-08-01 10:18:15 -0400

    Update PannelFour.tsx

--- src/components/FAQ/PannelFour.tsx
+++ src/components/FAQ/PannelFour.tsx
@@ -7,14 +7,14 @@ const PannelFour = () => {
       <Text fontFamily="Neue Montreal" fontWeight="400" whiteSpace="pre-wrap">
         All members of the community must go through a KYC/Proof of Humanity
         check. Additionally, subDAOs may have additional credentialing
-        requirements. Providers will be credentialed using a 3rd party provider,
-        once the provider has been approved a non-transferrable NFT will be
-        minted to their wallet enabling their entry into the proper subDAO.
-        Please click&nbsp;
+        requirements. Healthcare professionals will be credentialed using 
+        a 3rd party provider, once a healthcare professional has been approved
+        a non-transferrable NFT will be minted to their wallet, enabling their
+        entry into the proper subDAO. Please click&nbsp;
         <Link href="https://docs.meddao.xyz/" color="#FF003F">
         here
         </Link>{" "}
-        for more information,
+        for more information.
       </Text>
     </AccordionPanel>
   );
