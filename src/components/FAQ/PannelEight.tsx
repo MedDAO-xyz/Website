d1a4d7431c4656f8a01dfe7578d093d8db0b6899
ryan-meddao <105010559+ryan-meddao@users.noreply.github.com>
2022-08-15 12:01:00 -0400

    Update PannelEight.tsx

--- src/components/FAQ/PannelEight.tsx
+++ src/components/FAQ/PannelEight.tsx
@@ -5,12 +5,12 @@ const PannelEight = () => {
   return (
     <AccordionPanel mt=".5em" bg="#FFF9FB" pb={4}>
       <Text fontFamily="Neue Montreal" fontWeight="400" whiteSpace="pre-wrap">   
-        If we’ve piqued your interest, we encourage you to learn more by reading our 
-        <Link href="https://docs.meddao.xyz/" color="#FF003F">
-         documentation,
-        </Link>{" "}subscribing to our newsletter (below) and joining our           
-        <Link href="https://discord.com/invite/qwWATcETDq" color="#FF003F">
-         Discord server.
+        If we’ve piqued your interest, we encourage you to learn more by reading our
+        {" "}<Link href="https://docs.meddao.xyz/" color="#FF003F">
+        documentation
+        </Link>,{" "}subscribing to our newsletter (below) and joining our           
+        {" "}<Link href="https://discord.com/invite/qwWATcETDq" color="#FF003F">
+        Discord server.
         </Link>{" "}(our initial primary engagement platform).
       </Text>
     </AccordionPanel>
